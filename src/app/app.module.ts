f304d635f59e70e6fa69de11409619f6eba231e2
JFmaia <jfmaia741@gmail.com>
2023-05-16 15:00:21 -0300

    feat: Adicionando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextIntroComponent } from './text-intro/text-intro.component';
+import { CardLoginComponent } from './card-login/card-login.component';
+import { HomeComponent } from './page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextIntroComponent,
+    CardLoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
