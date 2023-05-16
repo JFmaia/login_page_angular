b002c0bb5076715271d6db25b40b6437661e0c7c
JFmaia <jfmaia741@gmail.com>
2023-05-16 15:02:14 -0300

    feat: Novo commponente

---
+++ src/app/text-intro/text-intro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-intro',
+  templateUrl: './text-intro.component.html',
+  styleUrls: ['./text-intro.component.scss']
+})
+export class TextIntroComponent {
+
+}
